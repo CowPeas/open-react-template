df3dadbe121b829330c42266d0153fe778f6e65a
Derrick Macharia <62021900+CowPeas@users.noreply.github.com>
2024-04-05 09:06:49 +0300

    Update features.tsx

--- components/features.tsx
+++ components/features.tsx
@@ -85,8 +85,8 @@ export default function Features() {
                   <path className="stroke-current text-purple-100" d="M8.86 1.18C3.8 1.988 0 5.6 0 10c0 5 4.9 9 11 9a10.55 10.55 0 003.1-.4L20 21l-.6-5.2a9.125 9.125 0 001.991-2.948" />
                 </g>
               </svg>
-              <h4 className="h4 mb-2">AI-Powered Instant Features</h4>
-              <p className="text-lg text-gray-400 text-center">Access a range of instant AI-powered features for seamless content creation.</p>
+              <h4 className="h4 mb-2">Chat-Based Prompting </h4>
+              <p className="text-lg text-gray-400 text-center">Access a range of instant AI-powered features for seamless content creation using prompts</p>
             </div>
 
           </div>
